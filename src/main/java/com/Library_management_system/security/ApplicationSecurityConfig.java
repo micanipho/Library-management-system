8137b61995bbcdb6efa6514ad99ac80cceb3cf4e
nipho <nqmasilela777@gmail.com>
2025-03-13 04:52:42 +0200

    implemented security basis auth

--- src/main/java/com/Library_management_system/security/ApplicationSecurityConfig.java
+++ src/main/java/com/Library_management_system/security/ApplicationSecurityConfig.java
@@ -1,22 +1,31 @@
 package com.Library_management_system.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/admin/**", "/", "index").permitAll()
+                                .requestMatchers("/admin/**", "/", "index.html").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
@@ -24,4 +33,26 @@ public class ApplicationSecurityConfig {
         return http.build();
     }
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails user = User.builder()
+                .username("nipho")
+                .password(passwordEncoder.encode("test"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user1 = User.builder()
+                .username("lee")
+                .password(passwordEncoder.encode("test"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(user, user1);
+    }
 }
---
+++ src/main/java/com/Library_management_system/security/ApplicationUserPermission.java
@@ -0,0 +1,4 @@
+package com.Library_management_system.security;
+
+public enum ApplicationUserPermission {
+}

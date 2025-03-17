fdbe5f0b1fb2a07a3d75f789ee00e21cd6aa9536
nhmasiljhb024 <nhmasiljhb024@student.wethinkcode.co.za>
2025-03-17 12:27:08 +0200

    just

--- src/main/java/com/Library_management_system/security/ApplicationSecurityConfig.java
+++ src/main/java/com/Library_management_system/security/ApplicationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.Library_management_system.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
